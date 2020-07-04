42868968a1e82b1c246d5c29939baa434a76746d
SorenA <s.dev@lysdal.org>
2020-07-04 20:11:08 +0200

    Add navigation models and mappings.

---
+++ src/LightOps.Commerce.Gateways.Storefront/Api/Models/INavigationLink.cs
@@ -0,0 +1,9 @@
+﻿namespace LightOps.Commerce.Gateways.Storefront.Api.Models
+{
+    public interface INavigationLink
+    {
+        string Title { get; set; }
+        string Url { get; set; }
+        string Target { get; set; }
+    }
+}
